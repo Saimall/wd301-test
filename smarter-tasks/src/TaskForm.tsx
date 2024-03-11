bc9a71646fca3192e2e19b5f4309b421c1df7153
Kiyotaka-Ayanak0ji <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2024-03-11 21:53:07 +0530

    Added refs

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -3,19 +3,25 @@ interface TaskFormProps {
   
 }
 interface TaskFormState {
+  title: string;
 }
+
 class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
   constructor(props: TaskFormProps) {
     super(props);
+    this.state = {
+      title: ""
+    }
   }
+  inputRef = React.createRef<HTMLInputElement>();
   addTask: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
-    console.log("Submitted the form!");
+    console.log(`Submitted the form with ${this.inputRef.current?.value}`);
   };
   render(){
     return (
       <form onSubmit={this.addTask}>
-        <input className="border-2 border-slate-500 rounded-lg w-1/6" type="text" />
+        <input className="border-2 border-slate-500 rounded-lg w-1/6" type="text" ref={this.inputRef} />
         <button className="border-4 border-purple-700 rounded-lg mx-auto w-1/5" type="submit">Add item</button>
       </form>
     )
