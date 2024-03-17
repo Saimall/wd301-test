fc2842cf3753a6c1b3b50393fc0156d5b4bb6983
Kiyotaka-Ayanak0ji <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2024-03-17 11:17:16 +0530

    Styles

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -58,7 +58,7 @@ const TaskForm = (props: TaskFormProps) => {
             required />
           <label
             htmlFor="todoTitle"
-            className="peer-focus:font-medium absolute font-bold text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0"
+            className="peer-focus:font-medium absolute font-bold text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Todo Title
           </label>
@@ -75,7 +75,7 @@ const TaskForm = (props: TaskFormProps) => {
             placeholder="" />
           <label
             htmlFor="todoDescription"
-            className="peer-focus:font-medium absolute font-bold text-l text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0"
+            className="peer-focus:font-medium absolute font-bold text-l text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Todo Description
           </label>
@@ -93,7 +93,7 @@ const TaskForm = (props: TaskFormProps) => {
             required />
           <label
             htmlFor="todoDueDate"
-            className="peer-focus:font-medium absolute font-bold text-l text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0"
+            className="peer-focus:font-medium absolute font-bold text-l text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Todo DueDate
           </label>
