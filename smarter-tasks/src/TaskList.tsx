63f8e0a7d146fb0b68bb4c84772c8221eb462845
Kiyotaka-Ayanak0ji <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2024-03-17 13:45:13 +0530

    modified

--- smarter-tasks/src/TaskList.tsx
+++ smarter-tasks/src/TaskList.tsx
@@ -3,14 +3,14 @@ import Task from "./Task";
 import { TaskItem } from "./types";
 
 interface Props {
+  tasks: TaskItem[];
 }
 interface State {
-  tasks: TaskItem[];
 }
 class TaskList extends React.Component<Props, State> {
 
   render() {
-    return this.state.tasks.map((task, idx) => (
+    return this.props.tasks.map((task, idx) => (
       <Task key={idx} title={task.title} dueDate={task.dueDate || "17-03-2004"} description={task.description || ""} />
     ));
   }
