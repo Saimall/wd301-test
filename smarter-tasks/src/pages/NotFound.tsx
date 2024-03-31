c35be375efd0163046fc94627c4af4fbdb30b400
Kiyotaka-Ayanak0ji <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2024-03-31 12:44:03 +0530

    Styled notfound

--- smarter-tasks/src/pages/NotFound.tsx
+++ smarter-tasks/src/pages/NotFound.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const NotFound: React.FC = () => {
 
     return (
-        <div className="flex flex-col justify-center items-center h-screen bordered border-purple-500 border-2 w-64">
+        <div className="flex flex-col justify-center items-center h-screen bordered border-purple-500 border-2 w-full">
             <p className="justify-center w-fit"> <b>404</b> The requested route was not found</p>
-            <button className="bg-green-300 m-2 p-2 rounded-md text-white font-bold" id="backToHomeButton">
+            <button className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-white-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" id="backToHomeButton">
                 <a href="/home">Go to home</a>
             </button>
         </div>
