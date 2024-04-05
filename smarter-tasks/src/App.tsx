92aa660b3269bb627bbca44d4b6fe21650252035
Kiyotaka-Ayanak0ji <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2024-04-05 21:52:17 +0530

    Modified Routes Layouts and Home Page

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,55 +1,56 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Signup from "./pages/signup";
-import Signin from "./pages/signin";
-import HomePage from "./pages/HomePage";
-import ProtectedRoute from "./ProtectedRoute";
-import NotFound from "./pages/Notfound";
-import Dashboard from "./pages/dashboard";
+import { RouterProvider} from "react-router-dom";
+// import Signup from "./pages/signup";
+// import Signin from "./pages/signin";
+// import HomePage from "./pages/HomePage";
+// import ProtectedRoute from "./ProtectedRoute";
+// import NotFound from "./pages/Notfound";
+// import Dashboard from "./pages/dashboard";
+import router from "./routes";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Signup />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/signin",
-    element: <Signin />,
-  },
-  {
-    path: "/notfound",
-    element: <NotFound />
-  },
-  {
-    path: "/dashboard",
-    element: (
-      <ProtectedRoute>
-        <Dashboard />
-      </ProtectedRoute>
-    ),
-    children: [
-      {
-        path: "home",
-        element: <HomePage />,
-      }
-    ],
-  },
-  {
-    path: "*",
-    element: <NotFound />
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Signup />,
+//   },
+//   {
+//     path: "/signup",
+//     element: <Signup />,
+//   },
+//   {
+//     path: "/signin",
+//     element: <Signin />,
+//   },
+//   {
+//     path: "/notfound",
+//     element: <NotFound />
+//   },
+//   {
+//     path: "/dashboard",
+//     element: (
+//       <ProtectedRoute>
+//         <Dashboard />
+//       </ProtectedRoute>
+//     ),
+//     children: [
+//       {
+//         path: "home",
+//         element: <HomePage />,
+//       }
+//     ],
+//   },
+//   {
+//     path: "*",
+//     element: <NotFound />
+//   }
+// ]);
 
 
-function App() {
+
+const App = () => {
   return (
     <div>
       <RouterProvider router={router} />
     </div>
   );
 }
-
 export default App;
