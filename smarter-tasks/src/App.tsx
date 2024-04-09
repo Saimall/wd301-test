4c648227213736f432b651747a01c4374cbb13cb
Kiyotaka-Ayanak0ji <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2024-04-09 09:55:51 +0530

    Added context action and reducer for projects

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -10,6 +10,7 @@ import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./routes"
 import {ThemeContext} from "./context/theme";
+import { ProjectsProvider } from "./context/projects/context";
 
 // const router = createBrowserRouter([
 //   {
@@ -54,9 +55,10 @@ const App = () => {
   const { theme } = useContext(ThemeContext)
   return (
     <div className={`h-screen w-full mx-auto py-2 ${theme === "dark" ? "dark" : ""}`}>
-      {theme}
-      <RouterProvider router={router} />
+      <ProjectsProvider>
+        <RouterProvider router={router} />
+      </ProjectsProvider>
     </div>
-  );
+  )
 }
 export default App;
