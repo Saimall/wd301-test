c490213ed4d44979c8ad47d3d31fb3cb46f3ad34
Kiyotaka-Ayanak0ji <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2024-04-20 08:31:14 +0530

    Final

--- smarter-tasks/src/pages/Notfound.tsx
+++ smarter-tasks/src/pages/Notfound.tsx
@@ -6,7 +6,7 @@ const Notfound = () => {
     <div>
       <h1>404 - Not Found</h1>
       <p>The page you are looking for does not exist.</p>
-      <Link to="/home">
+      <Link to="/signin">
         <button id="backToHomeButton" className="bg-blue-500 text-white py-2 px-4 mb-3 rounded hover:bg-blue-700">Back to Homepage</button>
       </Link>
     </div>
